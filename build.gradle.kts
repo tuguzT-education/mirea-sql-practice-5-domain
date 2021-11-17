6e85be4ac1fef67f1061cb2220cde3510a5ab0af
tuguzT <timurka.tugushev@gmail.com>
2021-11-17 21:11:53 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `java-library`
+    `maven-publish`
+    kotlin("jvm") version "1.6.0"
+}
+
+group = "io.github.tuguzt.sql"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+kotlin {
+    explicitApi()
+}
+
+dependencies {
+    // Kotlin
+    implementation(kotlin("stdlib-jdk8"))
+
+    // Testing
+    testImplementation(kotlin("test"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("jitpack") {
+            groupId = "com.github.tuguzT"
+            artifactId = "sql_practice_5_domain"
+
+            from(components["kotlin"])
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "SQLPractice5Domain"
